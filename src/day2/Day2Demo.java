2a9b1e6a03f113aa9d5636eec6266a40a3687c92
wedis <wedis@10.0.0.91>
2023-04-07 20:31:07 -0400

    Implemented GetInfo

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -2,10 +2,7 @@ package day2;
 
 public class Day2Demo {
 	
-	public static void main(String[] args) {
-		
-		System.out.println("Day 2 Git");
-		
+	public void getInfo() {
+		System.out.println("Pushing code...");
 	}
-
 }
